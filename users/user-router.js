dff054b523c1a268dc2a777d84050aae63b80a2e
tatiana faramarzi <tfaramarzi@gmail.com>
2019-09-11 10:51:23 -0700

    fixed join bug to fetch unique post ids

--- users/user-router.js
+++ users/user-router.js
@@ -80,9 +80,10 @@ router.delete('/:id', (req, res) => {
 router.get('/:id/posts', (req, res) => {
   const { id } = req.params;  
   // select * from posts where user_id = 123
-  db('posts as p')
-    .join('users as u', 'u.id', '=', 'p.user_id')
+  db('users as u')
+    .join('posts as p', 'u.id', '=', 'p.user_id')
     .where({ user_id: id })
+    //.select('p.id', 'p.contents', 'u.username')
     .then(posts => {
       res.status(200).json(posts);
     })
